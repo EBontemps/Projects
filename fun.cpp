d2b790822a717663307fd5913b670d9a1988cf19
EBontemps <45703401+EBontemps@users.noreply.github.com>
2018-12-07 18:05:32 -0800

    Create fun.cpp

---
+++ fun.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstreama>
+
+using namespace std;
+
+int main () {
+// declaring variables 
+int i;
+int j;
+
+cout << "Hello welcome" << endl;
+
+for(i = 0; i < 10; i++){
+cout << i + 5 << " ";
+}
